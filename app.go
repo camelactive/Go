2062a84d5d3cc9af5cefb3c62a48ca90910a44b2
Igor <camelactive@inbox.ru>
2026-01-12 20:52:54 +0500

    Добавил библиотеку figure

--- app.go
+++ app.go
@@ -1,12 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/common-nighthawk/go-figure" // Убедись, что эта строка тут есть
+)
 
 func main() {
 	// В Go мы используем := для краткого объявления и инициализации
 	greetings := "Hello world"
+	myFigure := figure.NewFigure(greetings, "", true)
+	myFigure.Print()
 	// Пакет fmt — это стандарт для ввода/вывода
-	fmt.Println(greetings)
+	// fmt.Println(greetings)
 	fmt.Println("\nНажмите Enter, чтобы выйти...")
 	fmt.Scanln()
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module app
 
 go 1.25.5
+
+require github.com/common-nighthawk/go-figure v0.0.0-20210622060536-734e95fb86be // indirect
